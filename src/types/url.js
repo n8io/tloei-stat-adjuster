d2259e83ac84d99e62c08b578c7b21b8b9eebcd6
Nate Clark <n8@n8io.com>
2024-09-12 21:07:55 -0400

    ✨ Update espn api urls

--- src/types/url.js
+++ src/types/url.js
@@ -14,16 +14,16 @@ const hydrate = rawUrl =>
 
 const Url = {
   API_LEAGUE_SETTINGS:
-    'https://fantasy.espn.com/apis/v3/games/ffl/seasons/{{seasonId}}/segments/0/leagues/{{leagueId}}',
+    'https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/{{seasonId}}/segments/0/leagues/{{leagueId}}',
   API_PLAYERS:
-    'https://fantasy.espn.com/apis/v3/games/ffl/seasons/{{seasonId}}/players?scoringPeriodId={{weekId}}',
+    'https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/{{seasonId}}/players?scoringPeriodId={{weekId}}',
   API_SCORE_ADJUSTMENT:
     'https://lm-api-writes.fantasy.espn.com/apis/v3/games/ffl/seasons/{{seasonId}}/segments/0/leagues/{{leagueId}}/schedule',
   LOGIN: 'http://www.espn.com/login',
   SCOREBOARD:
-    'https://fantasy.espn.com/football/league/scoreboard?leagueId={{leagueId}}&matchupPeriodId={{weekId}}',
+    'https://lm-api-reads.fantasy.espn.com/football/league/scoreboard?leagueId={{leagueId}}&matchupPeriodId={{weekId}}',
   SETTINGS:
-    'https://fantasy.espn.com/football/league/settings?leagueId={{leagueId}}&seasonId={{seasonId}}',
+    'https://lm-api-reads.fantasy.espn.com/football/league/settings?leagueId={{leagueId}}&seasonId={{seasonId}}',
 };
 
 export { Url, hydrate };
